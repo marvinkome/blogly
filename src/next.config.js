084abedb44a33aa5a8cb53d38dad2b71502a2721
marvinkome <marvinkome@gmail.com>
2018-07-23 14:08:38 +0100

    use merge-file-webpack plugin to merge all css files

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,27 @@
 // next.config.js
+const MergeFilesPlugin = require('merge-files-webpack-plugin');
 const withLess = require('@zeit/next-less');
 const withCss = require('@zeit/next-css');
-module.exports = withLess(withCss());
+
+const baseConfig = {
+    distDir: 'dist',
+    webpack(config, options) {
+        const { isServer, dev } = options;
+        //ref: https://github.com/zeit/next-plugins/issues/21
+        if (!isServer && !dev) {
+            // Override next-css configuration
+            options.extractCSSPlugin.filename = 'static/[name].css';
+            // Merge all CSS in one file
+            config.plugins.push(
+                new MergeFilesPlugin({
+                    filename: 'static/style.css',
+                    test: /\.css/,
+                    deleteSourceFiles: true
+                })
+            );
+        }
+        return config;
+    }
+};
+
+module.exports = withCss(withLess(baseConfig));
